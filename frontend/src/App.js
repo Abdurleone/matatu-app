feff9aa458c0d7b3ba14f871c6a9015375f676e7
Abdurleone <abyyyusuf@gmail.com>
2025-04-24 15:23:06 +0300

    Amended

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home.js';
 import MatatuDetailPage from './pages/MatatuDetailPage.js';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/matatu/:id" component={MatatuDetailPage} />
-      </Switch>
     </Router>
   );
 };
