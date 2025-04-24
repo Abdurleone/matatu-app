bfa942136f5c9ad6cc86b7ffe63b56e7de6685df
Abdurleone <abyyyusuf@gmail.com>
2025-04-24 12:24:47 +0300

    Passenger model amendment

--- backend/models/Passenger.js
+++ backend/models/Passenger.js
@@ -1,26 +1,40 @@
+const mongoose = require('mongoose');
+
 const passengerSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-    },
-    phone: {
-      type: String,
-      required: true,
-    },
-    matatu: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Matatu',
-      required: true,
-    },
-    seatNumber: {
-      type: String,
-      required: true,
-    },
-    bookingDate: {
-      type: Date,
-      default: Date.now,
-    },
-  });
-  
-  module.exports = mongoose.model('Passenger', passengerSchema);
-    
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  seatNumber: {
+    type: String,
+    required: true,
+  },
+  matatuId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Matatu',
+    required: true,
+  },
+  routeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Route',
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+}, {
+  timestamps: true,
+});
+
+const Passenger = mongoose.models.Passenger || mongoose.model('Passenger', passengerSchema);
+
+module.exports = Passenger;
