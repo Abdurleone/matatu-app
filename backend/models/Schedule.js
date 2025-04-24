cf85117f951c807f18305e1fdc20ca3c682e2cbd
Abdurleone <abyyyusuf@gmail.com>
2025-04-24 12:02:43 +0300

    Update

--- backend/models/Schedule.js
+++ backend/models/Schedule.js
@@ -1,34 +1,29 @@
 const mongoose = require('mongoose');
 
-const scheduleSchema = new mongoose.Schema({
-  matatuId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Matatu',
-    required: true,
-  },
-  routeId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Route',
-    required: true,
-  },
-  departureTime: {
-    type: Date,
-    required: true,
-  },
-  destination: {
+const matatuSchema = new mongoose.Schema({
+  registrationNumber: {
     type: String,
     required: true,
+    unique: true
+  },
+  capacity: {
+    type: Number,
+    required: true
+  },
+  driver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Driver'
+  },
+  conductor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Conductor'
   },
   isActive: {
     type: Boolean,
-    default: true,
-  },
-  deleted: {
-    type: Boolean,
-    default: false,
+    default: true
   }
 }, {
-  timestamps: true,
+  timestamps: true
 });
 
-module.exports = mongoose.model('Schedule', scheduleSchema);
+module.exports = mongoose.model('Matatu', matatuSchema);
